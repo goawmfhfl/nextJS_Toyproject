be65a04b277b797e02fd3bac5105a0a82f230e65
jae <goawmfhfl1@naver.com>
2022-05-09 15:40:29 +0900

    Add: Redirect, ReWrite

--- nextjs-intro/next.config.js
+++ nextjs-intro/next.config.js
@@ -1,6 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// const API_KEY = process.env.API_KEY;
+const API_KEY = "720d589a66f56d0458b9e5729c82da42";
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/old-blog/:path*",
+        destination: "/new-riding-blog/:path*",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
