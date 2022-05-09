3fb69e849a90c577e22c3120ac26b3f67b70e2d3
jae <goawmfhfl1@naver.com>
2022-05-09 12:57:20 +0900

    Add: MakeSeo

--- nextjs-intro/pages/about.js
+++ nextjs-intro/pages/about.js
@@ -1,5 +1,8 @@
+import Seo from "../components/Seo";
+
 const about = () => (
   <div>
+    <Seo title={"About"} />
     <h1>About Page</h1>
   </div>
 );
--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -1,6 +1,9 @@
+import Seo from "../components/Seo";
+
 const Home = () => {
   return (
     <div>
+      <Seo title={"Home"} />
       <h1>Home</h1>
     </div>
   );
