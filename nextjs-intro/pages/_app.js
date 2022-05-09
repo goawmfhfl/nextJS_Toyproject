6fcf9cb50b2ad72dfaa4e163226d163b954bb32a
jae <goawmfhfl1@naver.com>
2022-05-09 12:49:46 +0900

    Add: Layout

--- nextjs-intro/pages/_app.js
+++ nextjs-intro/pages/_app.js
@@ -1,22 +1,9 @@
-// about,index 파일이 열리기전에 이 파일을 먼저 로딩한다.
-// Component
-// psageProps
-// about.js 컴포넌트를 실행하고 싶다면
-// next.js는 about.js 컴포넌트를 App의 첫번째 인수로 받아와서 실행시킨다.
-import Navbar from "../components/Navbar";
+import Layout from "../components/Layout";
 const App = ({ Component, pageProps }) => {
   return (
-    <div>
-      <Navbar />
+    <Layout>
       <Component {...pageProps} />
-      <style jsx>
-        {`
-          a {
-            color: white;
-          }
-        `}
-      </style>
-    </div>
+    </Layout>
   );
 };
 
