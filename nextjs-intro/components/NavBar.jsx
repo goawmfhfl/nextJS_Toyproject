46677e968075b80468107197e25c427f51a01c32
jae <goawmfhfl1@naver.com>
2022-05-10 10:18:22 +0900

    Add: NavBar

--- nextjs-intro/components/NavBar.jsx
+++ nextjs-intro/components/NavBar.jsx
@@ -3,9 +3,8 @@ import { useRouter } from "next/router";
 
 const NavBar = () => {
   const router = useRouter();
-  console.log(router);
   return (
-    <nav className="nav">
+    <nav>
       <div className="wrapper">
         <Link href={"/"}>
           <a className={router.pathname === "/" ? "active" : ""}>Home</a>
@@ -14,6 +13,30 @@ const NavBar = () => {
           <a className={router.pathname === "/about" ? "active" : ""}>About</a>
         </Link>
       </div>
+      <style jsx>{`
+        nav {
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(18, 8, 8, 0.3) 0px 30px 60px -30px;
+          background-color: orange;
+        }
+        .wrapper {
+          display: flex;
+          gap: 10px;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
+        }
+        .active {
+          color: tomato;
+        }
+      `}</style>
     </nav>
   );
 };
